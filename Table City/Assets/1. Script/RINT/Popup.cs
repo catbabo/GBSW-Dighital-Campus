792480e37922e1ba60ee6b57825a94816a2a0c1b
Rint0411 <95272570+Rint0411@users.noreply.github.com>
2023-05-11 16:46:37 +0900

    풀링 및 UI / 사운드

---
+++ Table City/Assets/1. Script/RINT/Popup.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Popup : MonoBehaviour
+{
+    [field : SerializeField]
+    public Transform buttonAll { get; set; }
+    [field: SerializeField]
+    public TextMeshProUGUI text { get; set; }
+
+}
