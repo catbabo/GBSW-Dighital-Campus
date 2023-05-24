28e7f4fe5952e725524d5728d5313980aca2c496
catbabo <catbabo83@naver.com>
2023-05-24 14:39:21 +0900

    방 입장 시 플레이어 생성 음성채팅 연결

--- Table City/Assets/1. Script/LCS/NetworkManager.cs
+++ Table City/Assets/1. Script/LCS/NetworkManager.cs
@@ -9,8 +9,7 @@ using Photon.Realtime;
 public class NetworkManager : MonoBehaviourPunCallbacks
 {
 
-	public TMP_InputField _NickName;
-	public GameObject canvas;
+	public Transform[] _SpawnPoint;
 
 	private void Awake()
 	{
@@ -18,26 +17,37 @@ public class NetworkManager : MonoBehaviourPunCallbacks
 		PhotonNetwork.SerializationRate = 30;
 	}
 
+	private void Start()
+	{
+		Connect();
+	}
+
 	// 서버 연결
 	public void Connect() => PhotonNetwork.ConnectUsingSettings();
 
 	public override void OnConnectedToMaster()
 	{
-		PhotonNetwork.LocalPlayer.NickName = _NickName.text;
+		//PhotonNetwork.LocalPlayer.NickName = _NickName.text;
 		PhotonNetwork.JoinOrCreateRoom("Room", new RoomOptions { MaxPlayers = 2 }, null);
 	}
 
 	// 캐릭터 소환
 	public void SpawnCharater()
 	{
-		PhotonNetwork.Instantiate("Player", Vector3.zero, Quaternion.identity);
+
+		// 캐릭터 한명이라도 나간다면 방폭
+
+		GameObject _player = PhotonNetwork.Instantiate("0. Player/PlayerPrefab",
+							 PhotonNetwork.IsMasterClient ? _SpawnPoint[0].position : _SpawnPoint[1].position, 
+							 PhotonNetwork.IsMasterClient ? _SpawnPoint[0].rotation : _SpawnPoint[1].rotation);
+
+		PhotonView _pv = _player.GetComponent<PhotonView>();
+		PhotonNetwork.LocalPlayer.NickName = _pv.ViewID.ToString();
 	}
 
 	// 서버룸 조인
 	public override void OnJoinedRoom()
 	{
-		canvas.SetActive(false);
-
 		SpawnCharater();
 	}
 
@@ -53,6 +63,6 @@ public class NetworkManager : MonoBehaviourPunCallbacks
 	// 서버 연결 해제시 실행
 	public override void OnDisconnected(DisconnectCause cause)
 	{
-		canvas.SetActive(true);
+		
 	}
 }
--- Table City/Assets/1. Script/LCS/NetworkPlayer.cs
+++ Table City/Assets/1. Script/LCS/NetworkPlayer.cs
@@ -12,9 +12,23 @@ public class NetworkPlayer : MonoBehaviourPunCallbacks, IPunObservable
 	public PhotonView _PV;
 	public TMP_Text _NickNameTMP;
 
-	private void Awake()
+	private void Start()
+	{
+		SetNick();
+		SpawnCamrea();
+	}
+
+	private void SetNick()
 	{
 		_NickNameTMP.text = _PV.IsMine ? PhotonNetwork.NickName : _PV.Owner.NickName;
+	}
+
+	private void SpawnCamrea()
+	{
+		if (_PV.IsMine)
+		{
+			Instantiate(Resources.Load<GameObject>("0. Player/PlayerCamera"), transform.position + Vector3.up, transform.rotation, gameObject.transform);
+		}
 	}
 
 	public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
