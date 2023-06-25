2197c1bd716a4c6a46b4c38a5566a22d36d63bd0
2422june <ck66661254@gmail.com>
2023-06-26 08:26:52 +0900

    플레이어와 자원 상호작용 작업중

--- Table City/Assets/1. Script/GKI/Object/Resource/InputBoxController.cs
+++ Table City/Assets/1. Script/GKI/Object/Resource/InputBoxController.cs
@@ -4,5 +4,15 @@ using UnityEngine;
 
 public class InputBoxController : ObjectBase
 {
+    public override void Init() { }
 
+    public override void Interact(VRController interactedHand) { }
+    public override void Interact(VRController interactedHand, Transform target) { }
+
+    public override void ExitInteract() { }
+
+    private void OnDropResource()
+    {
+
+    }
 }
--- Table City/Assets/1. Script/GKI/Object/Resource/PlayerBoxController.cs
+++ Table City/Assets/1. Script/GKI/Object/Resource/PlayerBoxController.cs
@@ -3,6 +3,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/* Todo
+ * 
+1. GM의  Asset 구조체와 Enum을 Dfine으로 옮기자
+2. 데이터 구조를 변경해보자 (struct말고 int형 배열로)
+3. 플레이어가 현재 어느 진영인지 어떻게 알 수 있나?
+4. 각각의 박스에서 자원을 관리하면 불편한가?
+5. 
+ * 
+ * */
+
 public class PlayerBoxController : ObjectBase
 {
     private bool _isMine;
@@ -13,6 +23,8 @@ public class PlayerBoxController : ObjectBase
     private string _sourceParentPath = "ResourceItems/", _sourcePath;
     private GameObject _resourcePrefab, _resourceInstant;
 
+    private string[] _resourceNameA = {"Wood", "Iron", "Paper", "Glass", "Uranium", "Missrill" };
+    private string[] _resourceNameB= {"Stone", "Coal", "Bettery", "Rubber", "Semiconductor", "FlyStone"};
 
     private void Start()
     {
@@ -39,15 +51,28 @@ public class PlayerBoxController : ObjectBase
             case Define.ResourseType.Wood:
                 if(Managers.system.asset.wood > 0)
                 {
-                    _sourcePath = _sourceParentPath + "Wood";
                 }
                 break;
         }
 
+        bool isPlayerATeam = true;
+        if(isPlayerATeam)
+        {
+            _sourcePath = _sourceParentPath + _resourceNameA[(int)_resourseType];
+        }
+        else
+        {
+            _sourcePath = _sourceParentPath + _resourceNameB[(int)_resourseType];
+        }
         _resourcePrefab = Resources.Load<GameObject>(_sourcePath);
         _resourceInstant = Instantiate(_resourcePrefab, target);
         _resourceInstant.transform.localPosition = Vector3.zero;
         _resourceInstant.transform.localRotation = Quaternion.identity;
+    }
+
+    private void OnGrabResource()
+    {
+
     }
 
     public override void ExitInteract()
