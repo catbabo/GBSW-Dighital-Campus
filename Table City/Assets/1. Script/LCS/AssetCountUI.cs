7ff1b66b53429d519fd6574da89d7d8fbf2e7566
catbabo <catbabo83@naver.com>
2023-07-11 16:11:41 +0900

    AssetCount

---
+++ Table City/Assets/1. Script/LCS/AssetCountUI.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class AssetCountUI : MonoBehaviour
+{
+    [SerializeField] private TMP_Text[] _assetCount;
+    public bool _PointA;
+
+	private void FixedUpdate()
+	{
+		if (_PointA)
+		{
+			for (int i = 0; i < 6; i++) { _assetCount[i].text = Managers.system.asset[i].ToString(); }
+		}
+		else
+		{
+			for (int i = 6; i < 12; i++) { _assetCount[i].text = Managers.system.asset[i].ToString(); }
+		}
+	}
+
+}
